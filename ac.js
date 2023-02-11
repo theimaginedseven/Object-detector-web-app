8ba5b4c059748c0820ce0e063de50793fc05f3f3
theimaginedseven <103391499+theimaginedseven@users.noreply.github.com>
2023-02-11 16:59:54 +1000

    Add files via upload

---
+++ ac.js
@@ -0,0 +1,34 @@
+img = "";
+status = "";
+
+function preload() {
+    img = loadImage ('ac.jpg')
+}
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420)
+    fill("#FFFF00");
+    text("AC", 70, 70);
+    noFill();
+    stroke("#FFFF00");
+    rect(60, 50, 320, 160);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+
+function gotResults(error, results) { 
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+}
---
+++ fruit.js
@@ -0,0 +1,26 @@
+status = "";
+img = "";
+function preload()
+{
+    img = loadImage('ac.jpg')
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResults(error, results) { 
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+}
